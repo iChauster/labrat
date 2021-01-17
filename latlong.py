78c5354f78fe99a649817e32f5d2efd18496de41
savpatpanda <savandrew1@gmail.com>
2021-01-17 02:41:09 -0500

    new final csv

--- latlong.py
+++ latlong.py
@@ -3,20 +3,17 @@ import googlemaps
 
 API_KEY = 'AIzaSyCO9FSJNcibybFZ42dCroyS_SA1EYxWsqI'
 
-df = pd.read_csv('locationAppended.csv')
+df = pd.read_csv('final.csv')
 
 gmaps = googlemaps.Client(key=API_KEY)
 
 for index, row in df.iterrows():
-
 	if index%10 == 0:
-		print("{index} are completed.".format(index = index))
+		print('{pos} are done.'.format(pos = index))
+	if index%100 == 0:
+		df.to_csv('final1.csv', index=False)
+	result = gmaps.geocode(row['Location'])[0]['geometry']['location']
+	df.loc[index, 'Latitude'] = result['lat']
+	df.loc[index, 'Longitude'] = result['lng']
 
-	try:
-		hosLoc = gmaps.geocode(row['Location'])[0]['geometry']['location']
-		df.loc[index, 'Latitude'] = hosLoc['lat']
-		df.loc[index, 'Latitude'] = hosLoc['lng']
-	except:
-		print(index)
-
-df.to_csv('locationAppended.csv', index=False)
+df.to_csv('final1.csv', index=False)
